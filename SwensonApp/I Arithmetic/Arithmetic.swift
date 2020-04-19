771d66d497bde6a980b521d6bd0d9c297c7862ed
A. Abdurrahman <ahmed.m.abdurrahman@gmail.com>
2020-04-19 12:07:44 +0200

    Refactoring groups names

---
+++ SwensonApp/I Arithmetic/Arithmetic.swift
@@ -0,0 +1,13 @@
+//
+//  Arithmetic.swift
+//  SwensonApp
+//
+//  Created by Ahmed Abdurrahman on 4/19/20.
+//  Copyright © 2020 SwensonHe. All rights reserved.
+//
+
+import Foundation
+
+/*
+
+ */
